215bd77a538eab173f70495ef84a6fc3f8cbd087
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2024-05-11 14:35:50 -0700

    working

--- src/app/contacts/contact-list/contact-item/contact-item.component.ts
+++ src/app/contacts/contact-list/contact-item/contact-item.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { Contact } from '../contact.model';
+import { Contact } from '../../contact.model';
 
 @Component({
-  selector: 'cms-contact-list',
-  templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.css'
+  selector: 'cms-contact-item',
+  templateUrl: './contact-item.component.html',
+  styleUrl: './contact-item.component.css'
 })
-export class ContactListComponent {
+
+export class ContactItemComponent {
   contacts: Contact[] = [
     new Contact(1,"R. Kent Jackson", "jacksonk@byui.edu","253-496-3771", "../../assets/images/jacksonk.jpg",null ),
     new Contact(2, "Rex Barzee", "barzeer@byui.edu", "208-496-3768","../../assets/images/barzeer.jpg",null)
