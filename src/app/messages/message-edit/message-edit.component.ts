24883546b677467eaa0a2f8e14ffce32b16712db
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2024-05-13 22:56:43 -0700

    Mostly working, just switches to the document page when a new message is created. Not sure why.

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.component.css'
 })
 export class MessageEditComponent {
+  @ViewChild('subject', { static: true }) subjectInputRef: ElementRef;
+  @ViewChild('msgText', { static: true }) msgTextInputRef: ElementRef;
 
+  @Output() addMessageEvent = new EventEmitter<Message>();
+
+  currentSender: string = 'Your Name'; // Replace with your name
+
+  onSendMessage() {
+    const subjectValue = this.subjectInputRef.nativeElement.value;
+    const msgTextValue = this.msgTextInputRef.nativeElement.value;
+
+    const newMessage = new Message(
+      '1', // You may want to generate a unique ID or handle this differently
+      subjectValue,
+      msgTextValue,
+      this.currentSender
+    );
+
+    this.addMessageEvent.emit(newMessage);
+    this.onClear();
+  }
+
+  onClear() {
+    this.subjectInputRef.nativeElement.value = '';
+    this.msgTextInputRef.nativeElement.value = '';
+  }
 }
--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.component.css'
 })
 export class MessageListComponent {
+  messages: Message[] = [
+    new Message('1', 'Test Subject 1', 'This is the first test message.', 'Sender1'),
+    new Message('2', 'Test Subject 2', 'This is the second test message.', 'Sender2'),
+    new Message('3', 'Test Subject 3', 'This is the third test message.', 'Sender3')
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
 }
