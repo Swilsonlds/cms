6a9c932008d14a39a4420eabba5949d4a3492282
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2024-05-18 20:31:28 -0700

    Mostly done

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output} from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './document-list.component.css'
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent: EventEmitter<Document> = new EventEmitter<Document>();
 
+  documents = [
+    new Document(1,'txt','empty dex doc','https//mylink.com', 'none'),
+    new Document(2,'pdf','pdf doc','https//mypdf.com', 'none')
+  ]
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
